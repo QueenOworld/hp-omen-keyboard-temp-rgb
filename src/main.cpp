aa0236c5e3c1af88263573ff7c2dd40727c94efb
QueenOworld <queenoworld@disroot.org>
2024-08-01 06:39:36 -0400

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -70,35 +70,26 @@ rgb_color hsvtorgb(hsv_color hsv) {
 }
 
 unsigned long rgbtoulong(unsigned char r, unsigned char g, unsigned char b) {
-    return ((r) << 16) + ((g) << 8) + (b);
+    return (r << 16) + (g << 8) + b;
 }
 
 int main() {
     while (true) {
-        std::fstream thermal0("/sys/class/thermal/thermal_zone0/temp",
-                              std::ios_base::in);
-        std::fstream thermal1("/sys/class/thermal/thermal_zone1/temp",
-                              std::ios_base::in);
-
-        std::string temp0;
-        std::string temp1;
-
-        if (thermal0.is_open()) {
-            thermal0 >> temp0;
-            thermal0.close();
-        }
-
-        if (thermal1.is_open()) {
-            thermal1 >> temp1;
-            thermal1.close();
-        }
-
-        int average = (stoi(temp0) + stoi(temp1)) / 2;
+        std::ifstream thermal0("/sys/class/thermal/thermal_zone0/temp");
+        std::ifstream thermal1("/sys/class/thermal/thermal_zone1/temp");
 
         std::ofstream zone00("/sys/devices/platform/hp-wmi/rgb_zones/zone00");
         std::ofstream zone01("/sys/devices/platform/hp-wmi/rgb_zones/zone01");
         std::ofstream zone02("/sys/devices/platform/hp-wmi/rgb_zones/zone02");
         std::ofstream zone03("/sys/devices/platform/hp-wmi/rgb_zones/zone03");
+
+        std::string temp0;
+        std::string temp1;
+
+        thermal0 >> temp0;
+        thermal1 >> temp1;
+
+        int average = (stoi(temp0) + stoi(temp1)) / 2;
 
         hsv_color color_hsv =
             hsv_color{(unsigned char)(348 - 4 * (average / 1000)), 255, 255};
@@ -107,27 +98,18 @@ int main() {
 
         unsigned long color = rgbtoulong(color_rgb.r, color_rgb.g, color_rgb.b);
 
-        std::stringstream stream;
+        std::ostringstream stream;
         stream << std::hex << color;
         std::string output(stream.str());
 
-        if (zone00.is_open()) {
-            zone00 << output;
-            zone00.close();
-        }
-        if (zone01.is_open()) {
-            zone01 << output;
-            zone01.close();
-        }
-        if (zone02.is_open()) {
-            zone02 << output;
-            zone02.close();
-        }
-        if (zone03.is_open()) {
-            zone03 << output;
-            zone03.close();
-        }
+        zone00 << output;
 
-        sleep(1);
+        zone01 << output;
+
+        zone02 << output;
+
+        zone03 << output;
+
+        sleep(2);
     }
 }
